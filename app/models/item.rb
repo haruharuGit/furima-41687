3b26bdfc11c2a0e359dbd27eed567aa294ea0a9f
haruharuGit <chiharu.fu.54@gmail.com>
2024-11-13 17:45:22 +0900

    ActiveHashのモデル作成とアソシエーション追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :postage
+  belongs_to :prefecture
+  belongs_to :shipping_date
+
   belongs_to :user
   has_one :order
   has_one_attached :image
@@ -6,11 +13,11 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name
     validates :description
-    validates :category_id
-    validates :condition_id
-    validates :postage_id
-    validates :prefecture_id
-    validates :shipping_date_id
+    validates :category_id, nunmericality: { other_than: 1, message: "can't be blank" }
+    validates :condition_id, nunmericality: { other_than: 1, message: "can't be blank" }
+    validates :postage_id, nunmericality: { other_than: 1, message: "can't be blank" }
+    validates :prefecture_id, nunmericality: { other_than: 1, message: "can't be blank" }
+    validates :shipping_date_id, nunmericality: { other_than: 1, message: "can't be blank" }
     validates :price
   end
 end
