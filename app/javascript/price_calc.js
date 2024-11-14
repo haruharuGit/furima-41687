732c344f7f16d30b4767999fdbbd9ae29922ddcd
haruharuGit <chiharu.fu.54@gmail.com>
2024-11-14 10:42:59 +0900

    販売価格の計算を追加

--- app/javascript/price_calc.js
+++ app/javascript/price_calc.js
@@ -1,3 +1,13 @@
-window.addEventListener( 'turbo:load', function(){
-  // ここから記述、価格計算を書く
-} );
+function price_calc (){
+  const itemPrice = document.getElementById("item-price");
+  itemPrice.addEventListener("input", () => {
+    const inputPrice = itemPrice.value;
+    const addTaxPrice = document.getElementById("add-tax-price");
+    const taxPrice = Math.floor(inputPrice * 0.1);
+    const profit = document.getElementById("profit");
+    addTaxPrice.innerHTML = taxPrice;
+    profit.innerHTML = Math.floor(inputPrice - taxPrice);
+  });
+};
+
+window.addEventListener( 'turbo:load', price_calc);
