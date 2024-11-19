36219552b3a119697ad0a0136faf62e61ad52bba
haruharuGit <chiharu.fu.54@gmail.com>
2024-11-19 17:29:16 +0900

    ordersコントローラーのindexアクションとcreateアクションを追加

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,23 @@
 class OrdersController < ApplicationController
   def index
+    @item = Item.find_by(params[:id])
+    @order_address = OrderAddress.new
   end
 
   def create
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_number, :phone_number,
+                                          :item_id).merge(user_id: current_user.id)
   end
 end
