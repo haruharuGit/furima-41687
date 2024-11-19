ede0eb7bf72c14dbe1e985d91a063c855147638b
haruharuGit <chiharu.fu.54@gmail.com>
2024-11-19 13:05:45 +0900

    Addressモデルにバリデーションとアソシエーションを追加

---
+++ app/models/address.rb
@@ -0,0 +1,11 @@
+class Address < ApplicationRecord
+  belongs_to :order
+
+  with_options presence: true do
+    validates :postal_code
+    validates :prefecture_id
+    validates :city
+    validates :house_number
+    validates :phone_number
+  end
+end
