40cafc6344aa07001495c5ca4f5e29a25c7849de
haruharuGit <chiharu.fu.54@gmail.com>
2024-11-20 15:49:53 +0900

    商品の購入状態によるボタンの表示を変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def item_editable?(item, user)
+    user_signed_in? && user.id == item.user.id
+  end
+
+  def item_purchasable?(item)
+    user_signed_in? && item.order.nil?
+  end
 end
