ec98a8a394fdbc02f8968d1e0099f3bdeff77d80
haruharuGit <chiharu.fu.54@gmail.com>
2024-11-20 11:47:26 +0900

    ordersコントローラーに決済処理を追加

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,6 +3,7 @@ class OrdersController < ApplicationController
   before_action :check_current_user, only: [:index, :create]
 
   def index
+    gon.public_key = ENV['PAYJP_PUBLIC_KEY']
     @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
   end
@@ -10,9 +11,11 @@ class OrdersController < ApplicationController
   def create
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
+      pay_item
       @order_address.save
       redirect_to root_path
     else
+      gon.public_key = ENV['PAYJP_PUBLIC_KEY']
       @item = Item.find(params[:item_id])
       render :index, status: :unprocessable_entity
     end
@@ -29,5 +32,15 @@ class OrdersController < ApplicationController
   def check_current_user
     @item = Item.find(params[:item_id])
     redirect_to root_path if current_user.id == @item.user_id
+  end
+
+  def pay_item
+    order_item = Item.find(order_params[:item_id])
+    Pay.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: order_item.price,
+      card: order_params[:token],
+      currency: 'jpy'
+    )
   end
 end
